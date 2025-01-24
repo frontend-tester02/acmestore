e4318d36931090848e802e8242c9f2188e3d454b
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2025-01-24 23:20:45 +0900

    Update product slug type

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -2,17 +2,15 @@ import ProductImage from './_components/product-image'
 import { getDetailedProduct } from '@/actions/product.action'
 import ProductInfo from './_components/product-info'
 
-type PageProps = {
-	params: {
-		slug: string
-	}
+type Props = {
+	params: Promise<{ slug: string }>
 }
 
-async function Page({ params }: PageProps) {
-	const { slug } = params
+async function Page({ params }: Props) {
+	const { slug } = await params
 	const productJSON = await getDetailedProduct(slug)
-
 	const product = JSON.parse(JSON.stringify(productJSON))
+
 	return (
 		<>
 			<div className='container mx-auto max-w-screen-2xl'>
